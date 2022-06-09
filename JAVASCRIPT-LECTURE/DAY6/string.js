5a4dd20346584063b71e234a81d49b31c4519bc1
sohee Park <thgml5143@hanmail.net>
2022-06-09 17:34:36 +0900

    끝말잇기게임..

---
+++ JAVASCRIPT-LECTURE/DAY6/string.js
@@ -0,0 +1,14 @@
+const myName = "오늘은 비가 옵니다. 우산을 안들고 왔어요";
+console.log(myName.length);
+console.log(myName.substring(4, 10));
+console.log(myName.split());
+console.log(myName.startsWith("오늘"));
+console.log(myName.endsWith("요"));
+console.log(myName.indexOf("우리"));
+console.log(myName.includes("우리"));
+
+const myNum = "1";
+console.log(myNum.padEnd(7, "*"));
+
+const greeting = "              Hello Javascript";
+console.log(greeting.trim());
